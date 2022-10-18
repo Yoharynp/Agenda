58241df575ffd4e0a15f0f69391b44aad6a9e4df
Yoharynp <yohary_antonio29@hotmail.com>
2022-10-17 23:37:42 -0400

    Agenda

---
+++ JS/index.js
@@ -0,0 +1,34 @@
+const tabla = document.querySelector('#lista-usuarios tbody');
+
+function Cargar() {
+    fetch('http://www.raydelto.org/agenda.php')
+        .then(respuesta => respuesta.json()) //Indicamos el formato en que se desea obtener la información
+        .then(usuarios => {
+            usuarios.forEach(usuario => {
+                const row = document.createElement('tr');
+                row.innerHTML += `
+                    <td>${usuario.nombre}</td>
+                    <td>${usuario.apellido}</td>
+                    <td>${usuario.telefono}</td>
+                    
+                `;
+                tabla.appendChild(row);
+
+            });
+        }) // 
+        .catch(error => console.log('Hubo un error : ' + error.message))
+}
+
+let contacto = {nombre: 'Carvajal', apellido: 'Ramirez', telefono: '555-666-7777'};
+fetch("http://www.raydelto.org/agenda.php", 
+{
+ method:'POST', 
+ body:JSON.stringify(contacto)}
+).then( res => res.json()).then(
+ (res) => 
+{
+ console.log(res);
+}
+);
+
+Cargar();
---
+++ README.md
@@ -0,0 +1,3 @@
+Esta es mi tarea de la agenda de contactor, adjunto captura de pantalla
+
+![Mi captura](../Agenda/png/Tarea.png)
